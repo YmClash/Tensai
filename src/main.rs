c07f124025b4203753a85c9fddb280382e8bf4a9
y_mc <momo.toure27@gmail.com>
2025-04-14 18:09:39 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,26 @@ fn main() {
     println!("Hello, Tensaï: A Genius Tensor Language!!!!!!");
     println!("\n");
 
-    // let code_sourde = r#"obj.data[start+ offset].proccess(x,y).data[index]"#;
-    let code_sourde1 = r#"array[1:10:2]"#;
-    let code_sourde2 = r#"array[:10]"#;
-    let code_sourde3 = r#"array[1:]"#;
-    let code_sourde4 = r#"array[1:10]"#;
-    let code_sourde5 = r#"pub tensor A = 10 "#;
-    let code_sourde6 = r#"@shape(2,2) tensor C = A @ B"#;
+    let code_source = r#"obj.data[start+ offset].proccess(x,y).data[index]"#;
+    let code_source1 = r#"array[1:10:2]"#;
+    let code_source2 = r#"array[:10]"#;
+    let code_source3 = r#"array[1:]"#;
+    let code_source4 = r#"array[1:10]"#;
+    let code_source5 = r#"pub tensor A = 10 "#;
+    let code_source6 = r#"@shape(2,2) tensor C = A @ B"#;
 
-    // let code_sourde7 = r#"pub tensor A = 10 for i in range(100) { if sum(A) > 100 {print("A est grand")} elif sum(A) < 100 {print("A est petit")}else{print("A est égal à 100")}}"#;
-    let code_sourde8 = r#"tensor A = [1.0, 2.0, 3.0]"#;
+    // let code_source7 = r#"pub tensor A = 10 for i in range(100) { if sum(A) > 100 {print("A est grand")} elif sum(A) < 100 {print("A est petit")}else{print("A est égal à 100")}}"#;
+    let code_source8 = r#"tensor A = [1.0, 2.0, 3.0]"#;
 
-    let code_sourde9 = r#"tensor B:f32 @shape(2,2,2) = [[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]]]"#;
-    let code_sourde10 = r#"fn dot_product(a:int, b:int) -> int { let mut x:i32 = 10;tensor B:f32 @shape(2,2,2) = [[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]]];return sum(A@B)}"#;
-    let code_sourde11= r#"let model = init_model(3);tensor X:f32 @shape(2,2,2) = [[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]]];let y = [1.0, 2.0];train(model, x, y, lr=0.01, epochs=100)"#;
+    let code_source9 = r#"tensor B:f32 @shape(2,2,2) = [[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]]]"#;
+    let code_source10 = r#"fn dot_product(a:int, b:int) -> int { let mut x:i32 = 10;tensor B:f32 @shape(2,2,2) = [[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]]];return sum(A@B)}"#;
+    let code_source11= r#"let model = init_model(3);tensor X:f32 @shape(2,2,2) = [[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]]];let y = [1.0, 2.0];train(model, x, y, lr=0.01, epochs=100)"#;
 
 
 
     // let code_sourde12= r#" if sum(A) > 100 {print("A est grand")} elif sum(A) < 100 {print("A est petit")}else{print("A est égal à 100");"#;
 
-    let mut lexer = Lexer::new(code_sourde9);
+    let mut lexer = Lexer::new(code_source);
     let tokens = lexer.tokenize();
 
 
