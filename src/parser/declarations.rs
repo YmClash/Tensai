fe4d0f7550a0867e37d173e8b3d3585338babc5b
y_mc <momo.toure27@gmail.com>
2025-04-14 19:01:09 +0200

    UPDATE

--- src/parser/declarations.rs
+++ src/parser/declarations.rs
@@ -67,9 +67,9 @@ impl Parser{
         let value = self.parse_expression(0)?;
 
 
-        let device = if self.match_token(&[TokenType::OPERATOR(Operators::AT)]){
-            Some(self.parse_device()?)
-        }else { None };
+        // let device = if self.match_token(&[TokenType::OPERATOR(Operators::AT)]){
+        //     Some(self.parse_device()?)
+        // }else { None };
 
 
         self.consume_seperator();
@@ -82,7 +82,8 @@ impl Parser{
             dtype,
             value,
             mutability,
-            visibility
+            visibility,
+            // device,
         })))
 
     }
